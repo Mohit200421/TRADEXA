348429ea484907a86113387db3d137f5f84e8047
Mohit Badgujar <144667403+Mohit200421@users.noreply.github.com>
2026-01-25 23:12:33 +0530

    server fix

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,8 @@ const app = express();
 
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173", "https://YOUR-VERCEL-URL.vercel.app"]
+}));
 app.use(express.json());
 app.use(cookieParser());
 
