9f976f730e9053e31d62a73e9442db0b6d02efeb
Mohit Badgujar <144667403+Mohit200421@users.noreply.github.com>
2026-01-31 15:13:31 +0530

    fikjbjbscv

--- Frontend/src/api/axios.ts
+++ Frontend/src/api/axios.ts
@@ -1,7 +1,9 @@
+// src/api/axios.ts
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
 
 API.interceptors.request.use((config) => {
