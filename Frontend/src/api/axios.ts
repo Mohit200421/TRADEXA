4aea28e5971a071fd8434884457e13f2990d6c88
Mohit Badgujar <144667403+Mohit200421@users.noreply.github.com>
2026-02-04 21:24:09 +0530

    dc

--- Frontend/src/api/axios.ts
+++ Frontend/src/api/axios.ts
@@ -3,22 +3,17 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "https://tradexa-backend.onrender.com/api", // ✅ MUST include /api
+    "https://tradefx-8njj.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-/* =========================
-   AUTH INTERCEPTOR
-========================= */
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-
   return config;
 });
 
